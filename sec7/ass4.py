3a8957cbe0bea2d076cb28658634198a210576f4
Karim Safan <110535652+karim1safan@users.noreply.github.com>
2022-12-27 21:04:12 +0200

    Create ass4.py

---
+++ sec7/ass4.py
@@ -0,0 +1,16 @@
+num_one = 10
+num_two = 20
+
+result = num_one + num_two
+print(result)
+
+exponent = pow(result,3)
+print(exponent)
+
+moduel = exponent % 26000
+print(moduel)
+
+division = float(moduel) / 5
+print(division)
+
+print(type(str(division)))
