f07416f0f348602f97eb3b578d626489aa1604d1
Karim Safan <110535652+karim1safan@users.noreply.github.com>
2022-12-27 21:26:17 +0200

    Create ass1.py

---
+++ sec9/ass1.py
@@ -0,0 +1,17 @@
+num_one = int(input("X: "))
+operation = input("Choose operator: ").strip()
+num_two = int(input("Y: "))
+
+match(operation):
+    case "+":
+        result = num_one + num_two
+        print(f"{num_one} + {num_two} = {result}")
+    case "-":
+        result = num_one - num_two
+        print(f"{num_one} - {num_two} = {result}")
+    case "*":
+        result = num_one + num_two
+        print(f"{num_one} * {num_two} = {result}")
+    case "/":
+        result = num_one + num_two
+        print(f"{num_one} / {num_two} = {result}")
