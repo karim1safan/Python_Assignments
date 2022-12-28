9531ede46ee8bb108375cc9a781c4f5c6e57c5f0
Karim Safan <110535652+karim1safan@users.noreply.github.com>
2022-12-28 14:11:06 +0200

    Create ass1.py

---
+++ sec4/ass1.py
@@ -0,0 +1,11 @@
+friends = ["Osama", "Ahmed", "Sayed", "Ali", "Mahmoud"]
+
+print(friends[0])
+print(friends[-5])
+print(friends[4])
+print(friends[-1])
+# Needed Output
+# "Osama" => Method One
+# "Osama" => Method Two
+# "Mahmoud" => Method One
+# "Mahmoud" => Method Two
