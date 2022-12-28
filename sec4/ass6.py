aadabff7c66070150ed5f012d2b70f6dc0812cb7
Karim Safan <110535652+karim1safan@users.noreply.github.com>
2022-12-28 14:26:42 +0200

    Create ass6.py

---
+++ sec4/ass6.py
@@ -0,0 +1,11 @@
+friends = ["Nasser", "Osama", "Ahmed", "Sayed", "Salem"]
+
+friends [0:2] = []
+print(friends)
+
+friends.pop()
+print(friends)
+
+# Needed Output
+# ["Ahmed", "Sayed", "Salem"]
+# ["Ahmed", "Sayed"]
