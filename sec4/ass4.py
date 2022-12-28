54990feac371ba14fd3813930a8613482f13c7fd
Karim Safan <110535652+karim1safan@users.noreply.github.com>
2022-12-28 14:20:40 +0200

    Create ass4.py

---
+++ sec4/ass4.py
@@ -0,0 +1,7 @@
+friends = ["Osama", "Ahmed", "Sayed", "Ali", "Mahmoud"]
+friends[-1] = "Elzero"
+friends[-2] = "Elzero"
+print(friends)
+
+# Needed Output
+# ["Osama", "Ahmed", "Sayed", "Elzero", "Elzero"]
