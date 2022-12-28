d6efa0be5745276c09b55e360732526fc5184788
Karim Safan <110535652+karim1safan@users.noreply.github.com>
2022-12-28 14:24:41 +0200

    Create ass5.py

---
+++ sec4/ass5.py
@@ -0,0 +1,11 @@
+friends = ["Osama", "Ahmed", "Sayed"]
+
+friends.insert(0,"Nasser")
+print(friends)
+
+friends.append("Salem")
+print(friends)
+
+# Needed Output
+# ["Nasser", "Osama", "Ahmed", "Sayed"]
+# ["Nasser", "Osama", "Ahmed", "Sayed", "Salem"]
