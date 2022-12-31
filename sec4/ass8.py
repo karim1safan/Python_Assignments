43d88915c9d651214c6e14b4fdf602739259377c
Karim Safan <110535652+karim1safan@users.noreply.github.com>
2022-12-31 19:17:24 +0200

    Create ass8.py

---
+++ sec4/ass8.py
@@ -0,0 +1,8 @@
+friends = ["Ahmed", "Sayed", "Samah", "Eman", "Ramy", "Shady"]
+
+print(sorted(friends))
+print(sorted(friends, reverse=True))
+
+# Needed Output
+# ['Ahmed', 'Eman', 'Ramy', 'Samah', 'Sayed', 'Shady']
+# ['Shady', 'Sayed', 'Samah', 'Ramy', 'Eman', 'Ahmed']
