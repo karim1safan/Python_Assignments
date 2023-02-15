38ae2f4fc784e08874982371eb567ddb24d631d8
Karim Safan <110535652+karim1safan@users.noreply.github.com>
2023-02-15 14:17:04 +0200

    Create ass3.py

---
+++ sec5/ass3.py
@@ -0,0 +1,9 @@
+nums = (1, 2, 3)
+letters = ("A", "B", "C")
+
+print(nums + letters)
+print(len(nums + letters), "Elements")
+
+# Needed Output
+# nums_and_letters_one = (1, 2, 3, "A", "B", "C")
+# 6 Elements
